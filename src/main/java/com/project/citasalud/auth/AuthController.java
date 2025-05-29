f72956442c8d3236d6cf4bca15e7d6e9ba665a95
Sebastian Berrio <sberrio09@gmail.com>
2025-05-28 20:32:16 -0500

    add cors config

--- src/main/java/com/project/citasalud/auth/AuthController.java
+++ src/main/java/com/project/citasalud/auth/AuthController.java
@@ -5,15 +5,13 @@ import com.project.citasalud.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class AuthController {
 
