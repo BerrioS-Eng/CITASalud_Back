262837954a54767b89c633213c6f54c60a92556b
Sebastian Berrio <sberrio09@gmail.com>
2025-06-16 22:58:28 -0500

    Change config cors

--- src/main/java/com/project/citasalud/config/SecurityConfig.java
+++ src/main/java/com/project/citasalud/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -31,7 +30,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable
                         )
-                .cors(Customizer.withDefaults())
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests( authRequest ->
                         authRequest
                                 .requestMatchers("/auth/**").permitAll()
