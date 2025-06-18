22f3b4234b46a2292f563dd2ee9972105e0f9179
Sebastian Berrio <sberrio09@gmail.com>
2025-06-17 19:32:33 -0500

    config cors

--- src/main/java/com/project/citasalud/config/SecurityConfig.java
+++ src/main/java/com/project/citasalud/config/SecurityConfig.java
@@ -63,7 +63,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
